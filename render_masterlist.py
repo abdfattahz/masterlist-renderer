488f2ed66e7d25c8da85590b4116610df272b2c4
abdfattahz <203197396+abdfattahz@users.noreply.github.com>
2026-01-10 14:55:47 +0800

    feat: add two new function
    one for getting each sheet's name
    another is for getting each data from each row and return it as tuples like this `('COMPANY NAME', 'COMPANY NO.')`

--- render_masterlist.py
+++ render_masterlist.py
@@ -1,9 +1,26 @@
 import pandas as pd
 
-path = 'masterlist.xlsx'
-xl = pd.ExcelFile(path)
+def get_sheet_names(path):
+    xl = pd.ExcelFile(path)
+    return xl.sheet_names
 
-for sheet in xl.sheet_names:
-    # `nrows=0` so that it will only read the header
-    df = pd.read_excel(path, sheet_name=sheet, nrows=0)
-    print(sheet, '->', df.columns.tolist())
+def load_rows_from_sheet(path, sheet_name):
+    df = pd.read_excel(path, sheet_name=sheet_name)
+    
+    # advanded compact way
+    # df.columns = [c.strip() for c in df.columns]
+
+    # long version
+    new_columns = []
+    for c in df.columns:
+        new_columns.append(c.strip())
+    
+    df.columns = new_columns
+    
+    df = df[['COMPANY NAME', 'COMPANY NO.']].dropna(how='all')
+
+    df['COMPANY NAME'] = df['COMPANY NAME'].astype(str).str.strip()
+    df['COMPANY NO.'] = df['COMPANY NO.'].astype(str).str.strip()
+    
+    # we do this so that each elaned rows will be output as tuples `('COMPANY NAME', 'COMPANY NO.')`
+    return list(df.itertuples(index=False, name=None))
